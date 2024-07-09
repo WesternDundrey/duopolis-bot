101a269ef53a9a7600083ae27746ad39e42a883d
westerndundrey <hijackedpuffin@gmail.com>
2024-07-10 07:49:12 +0800

    so many errors :/

--- src/app.rs
+++ src/app.rs
@@ -1,11 +1,21 @@
 use leptos::*;
 use leptos_meta::*;
 use leptos_router::*;
+
+use crate::model::conversation::Conversation;
+
 #[component]
-pub fn App() -> impl IntoView {
+pub fn App(cx: Scope) -> impl IntoView {
     // Provides context that manages stylesheets, titles, meta tags, etc.
     provide_meta_context();
+    let (conversation, set_conversation) = create_signal(cv, Conversation::new())
 
+    let send = create_action(cx, move |new_message: &String| {
+        let user_message = Message {
+            text:new_message.clone(),
+            user: true,
+        };
+        });
     view! { cx,
         // injects a stylesheet into the document <head>
         // id=leptos means cargo-leptos will hot-reload this stylesheet
--- src/model/conversation.rs
+++ src/model/conversation.rs
@@ -6,6 +6,14 @@ pub struct Conversation {
     pub messages: Vec<Message>
 }
 
+impl Conversation{
+    pub fn new( )-> Conversation{
+        Conversation{ 
+            messages:Vec::new()
+    }
+}
+}
+
 #[derive(Serialize,Deserialize, Clone, Debug)]
 pub struct Message {
     pub user: bool,
