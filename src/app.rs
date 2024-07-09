cfa76501e09d2e9c8273c0a6cbd33b431508c7e5
westerndundrey <hijackedpuffin@gmail.com>
2024-07-10 07:24:03 +0800

    steady

--- src/app.rs
+++ src/app.rs
@@ -7,38 +7,18 @@ pub fn App() -> impl IntoView {
     // Provides context that manages stylesheets, titles, meta tags, etc.
     provide_meta_context();
 
-    view! {
+    view! { cx,
         // injects a stylesheet into the document <head>
         // id=leptos means cargo-leptos will hot-reload this stylesheet
         <Stylesheet id="leptos" href="/pkg/duopolis-bot.css"/>
 
         // sets the document title
-        <Title text="sup brah"/>
+        <Title text="Rusty Llama"/>
 
         // content for this welcome page
-        <Router>
-            <main>
-                <Routes>
-                    <Route path="" view=HomePage/>
-                    <Route path="/*any" view=NotFound/>
-                </Routes>
-            </main>
-        </Router>
     }
 }
 
-/// Renders the home page of your application.
-#[component]
-fn HomePage() -> impl IntoView {
-    // Creates a reactive value to update the button
-    let (count, set_count) = create_signal(0);
-    let on_click = move |_| set_count.update(|count| *count += 1);
-
-    view! {
-        <h1>"Welcome to Leptos!"</h1>
-        <button on:click=on_click>"Click Me: " {count}</button>
-    }
-}
 
 /// 404 - Not Found
 #[component]
