4dca64c016fa690a593d52ec7b3b768d8667f1e5
westerndundrey <hijackedpuffin@gmail.com>
2024-07-10 08:25:13 +0800

    working on api utilizing previous history

--- src/api.rs
+++ src/api.rs
@@ -14,5 +14,46 @@ pub async fn converse( cx: Scope, prompt: Converation) -> Result<String, ServerF
     .await.unwrap();
 
     use llm::KnownModel;
-    let character_name 
+    let character_name = "### Assistant";
+    let user_name = "## Human";
+    let person = "A chat between a human and an assistant";
+    let mut history = format!(
+        "{character_name}:Hello - How may I help you today?\n\
+        {user_name}:What is the capital of France?\n\
+        {character_name}:Paris it eh capital of France.\n
+        
+        "
+    );
+
+    for message in prompt.messages.into_iter() {
+        let msg = message.text;
+        let curr_line = if message.user{
+            format!("{character_name}:{msg}\n")
+        } else{
+            format!("{user_name}: {msg}\n")
+        };
+        history.push_str(&curr_line);
+    }
+    let mut res = String::new();
+    let mut rng = rand::thread_rng();
+    let mut buf =  String::new();
+
+    let mut session = model.start_session(Default::default());
+
+    session.infer(
+        model.as_ref(),
+        &mut rng,
+        &llm::InferenceRequest {
+            prompt: format!("{persona}\n{history}\n{character_name}\n")
+            .as_str()
+            .into(),
+        parameters: Some(&llm::InferenceRequestParameters::default()),
+        play_back_previous_tokens: false,
+        maximum_token_count:None,
+        },
+        &mut Default::default(),
+        inference_callback(String::from(user_name), &mut buf, &mut res),
+    )
+    .unwrap_or_else(|e| panic!("{e}"));
+    Ok(String::from(""))
 } 
