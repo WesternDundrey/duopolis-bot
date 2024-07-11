ee1a9747722ccd99c2f6c0d150d155f1a387e952
westerndundrey <hijackedpuffin@gmail.com>
2024-07-11 08:56:58 +0800

    lots of changes

--- src/model/conversation.rs
+++ src/model/conversation.rs
@@ -1,20 +1,21 @@
+extern crate serde_derive;
+
 use serde::Deserialize;
 use serde::Serialize;
 
-#[derive(Serialize,Deserialize, Clone, Debug)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct Conversation {
     pub messages: Vec<Message>
 }
-
-impl Conversation{
-    pub fn new( )-> Conversation{
-        Conversation{ 
-            messages:Vec::new()
+impl Conversation {
+    pub fn new() -> Conversation{
+        Conversation {
+            messages: Vec::new(),
+        }
     }
 }
-}
 
-#[derive(Serialize,Deserialize, Clone, Debug)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct Message {
     pub user: bool,
     pub text: String,
